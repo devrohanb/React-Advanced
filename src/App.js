f60d0b3b26ff6e80ce0c9cf96a3264ccf7532d58
devrohanb <dev.rohanb@gmail.com>
2021-02-07 22:37:04 +0530

    After implementing Context API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import RefsDemo from "./Components/RefsDemo";
+import ComponentC from "./Components/ComponentC";
+import { UserProvider } from "./Components/userContext";
 
 function App() {
   return (
     <div className="App">
-      <RefsDemo />
+      <UserProvider value="Aditya">
+        <ComponentC />
+      </UserProvider>
     </div>
   );
 }
